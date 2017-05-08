219555ff12c97cec020730131a8052f1089b4530
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-05-08 02:13:45 -0400

    add video url in admin add video form and on video show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507202547) do
+ActiveRecord::Schema.define(version: 20170508051657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170507202547) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.string   "token"
+    t.string   "video_url"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,7 @@ Video.create(
   description: 'Friends is an American situation comedy about a group of friends living in the New York City borough of Manhattan. It was created by David Crane and Marta Kauffman, which premiered on NBC on September 22, 1994. The series was produced by Bright/Kauffman/Crane Productions, in association with Warner Bros. Television.',
   small_cover: Rails.root.join('public/uploads/friends.jpg').open,
   large_cover: Rails.root.join('public/uploads/friends_large.png').open,
-  category_id: 1
-)
-Video.create(
-  title: 'Monk',
-  description: 'Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk.',
-  small_cover: Rails.root.join('public/uploads/monk.jpg').open,
-  large_cover: Rails.root.join('public/uploads/monk_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=Xs-HbHCcK58',
   category_id: 1
 )
 Video.create(
@@ -20,6 +14,7 @@ Video.create(
   description: 'The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious, and is offered a chance to have his criminal history erased as payment for a seemingly impossible task: "inception", the implantation of another person\'s idea into a target\'s subconscious.',
   small_cover: Rails.root.join('public/uploads/inception.jpg').open,
   large_cover: Rails.root.join('public/uploads/inception_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=YoHD9XEInc0',
   category_id: 2
 )
 Video.create(
@@ -27,6 +22,7 @@ Video.create(
   description: 'Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius. Before his death, the Emperor chooses Maximus to be his heir over his own son, Commodus, and a power struggle leaves Maximus and his family condemned to death.',
   small_cover: Rails.root.join('public/uploads/gladiator.jpg').open,
   large_cover: Rails.root.join('public/uploads/gladiator_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=owK1qxDselE',
   category_id: 2
 )
 Video.create(
@@ -34,6 +30,7 @@ Video.create(
   description: 'Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk.',
   small_cover: Rails.root.join('public/uploads/monk.jpg').open,
   large_cover: Rails.root.join('public/uploads/monk_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=L_IOsLYVKkY',
   category_id: 1
 )
 Video.create(
@@ -41,6 +38,7 @@ Video.create(
   description: 'Friends is an American situation comedy about a group of friends living in the New York City borough of Manhattan. It was created by David Crane and Marta Kauffman, which premiered on NBC on September 22, 1994. The series was produced by Bright/Kauffman/Crane Productions, in association with Warner Bros. Television.',
   small_cover: Rails.root.join('public/uploads/friends.jpg').open,
   large_cover: Rails.root.join('public/uploads/friends_large.png').open,
+  video_url: 'https://www.youtube.com/watch?v=Xs-HbHCcK58',
   category_id: 1
 )
 Video.create(
@@ -48,6 +46,7 @@ Video.create(
   description: 'The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious, and is offered a chance to have his criminal history erased as payment for a seemingly impossible task: "inception", the implantation of another person\'s idea into a target\'s subconscious.',
   small_cover: Rails.root.join('public/uploads/inception.jpg').open,
   large_cover: Rails.root.join('public/uploads/inception_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=YoHD9XEInc0',
   category_id: 2
 )
 Video.create(
@@ -55,6 +54,7 @@ Video.create(
   description: 'Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius. Before his death, the Emperor chooses Maximus to be his heir over his own son, Commodus, and a power struggle leaves Maximus and his family condemned to death.',
   small_cover: Rails.root.join('public/uploads/gladiator.jpg').open,
   large_cover: Rails.root.join('public/uploads/gladiator_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=owK1qxDselE',
   category_id: 2
 )
 Video.create(
@@ -62,6 +62,7 @@ Video.create(
   description: 'Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk.',
   small_cover: Rails.root.join('public/uploads/monk.jpg').open,
   large_cover: Rails.root.join('public/uploads/monk_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=L_IOsLYVKkY',
   category_id: 1
 )
 Video.create(
@@ -69,6 +70,7 @@ Video.create(
   description: 'Friends is an American situation comedy about a group of friends living in the New York City borough of Manhattan. It was created by David Crane and Marta Kauffman, which premiered on NBC on September 22, 1994. The series was produced by Bright/Kauffman/Crane Productions, in association with Warner Bros. Television.',
   small_cover: Rails.root.join('public/uploads/friends.jpg').open,
   large_cover: Rails.root.join('public/uploads/friends_large.png').open,
+  video_url: 'https://www.youtube.com/watch?v=Xs-HbHCcK58',
   category_id: 1
 )
 Video.create(
@@ -76,6 +78,7 @@ Video.create(
   description: 'The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious, and is offered a chance to have his criminal history erased as payment for a seemingly impossible task: "inception", the implantation of another person\'s idea into a target\'s subconscious.',
   small_cover: Rails.root.join('public/uploads/inception.jpg').open,
   large_cover: Rails.root.join('public/uploads/inception_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=YoHD9XEInc0',
   category_id: 2
 )
 Video.create(
@@ -83,6 +86,7 @@ Video.create(
   description: 'Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius. Before his death, the Emperor chooses Maximus to be his heir over his own son, Commodus, and a power struggle leaves Maximus and his family condemned to death.',
   small_cover: Rails.root.join('public/uploads/gladiator.jpg').open,
   large_cover: Rails.root.join('public/uploads/gladiator_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=owK1qxDselE',
   category_id: 2
 )
 Video.create(
@@ -90,6 +94,7 @@ Video.create(
   description: 'Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk.',
   small_cover: Rails.root.join('public/uploads/monk.jpg').open,
   large_cover: Rails.root.join('public/uploads/monk_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=L_IOsLYVKkY',
   category_id: 1
 )
 Video.create(
@@ -97,6 +102,7 @@ Video.create(
   description: 'Friends is an American situation comedy about a group of friends living in the New York City borough of Manhattan. It was created by David Crane and Marta Kauffman, which premiered on NBC on September 22, 1994. The series was produced by Bright/Kauffman/Crane Productions, in association with Warner Bros. Television.',
   small_cover: Rails.root.join('public/uploads/friends.jpg').open,
   large_cover: Rails.root.join('public/uploads/friends_large.png').open,
+  video_url: 'https://www.youtube.com/watch?v=Xs-HbHCcK58',
   category_id: 1
 )
 Video.create(
@@ -104,6 +110,7 @@ Video.create(
   description: 'The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious, and is offered a chance to have his criminal history erased as payment for a seemingly impossible task: "inception", the implantation of another person\'s idea into a target\'s subconscious.',
   small_cover: Rails.root.join('public/uploads/inception.jpg').open,
   large_cover: Rails.root.join('public/uploads/inception_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=YoHD9XEInc0',
   category_id: 2
 )
 Video.create(
@@ -111,6 +118,7 @@ Video.create(
   description: 'Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius. Before his death, the Emperor chooses Maximus to be his heir over his own son, Commodus, and a power struggle leaves Maximus and his family condemned to death.',
   small_cover: Rails.root.join('public/uploads/gladiator.jpg').open,
   large_cover: Rails.root.join('public/uploads/gladiator_large.jpg').open,
+  video_url: 'https://www.youtube.com/watch?v=owK1qxDselE',
   category_id: 2
 )
 
